a7debc4c4255eccf4e82aa56073b4874d85e4842
jlawyo <jcbyounger@gmail.com>
2026-09-18 17:47:01 +0000

    fix http server

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,16 @@
-use axum::Server;
 use std::net::SocketAddr;
 use hello_cargo::app;
 
 #[tokio::main]
 async fn main() {
-    // build our application with a single route
     let app = app();
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("Listening on {}", addr);
-    Server::bind(&addr)
-        .serve(app.into_make_service())
+
+    let listener = tokio::net::TcpListener::bind(addr)
         .await
         .unwrap();
 
-    // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
