765f45b26f907ea266f61ec3db806c32e239b405
jlawyo <jcbyounger@gmail.com>
2026-09-18 17:10:59 +0000

    Set up part of Unit Test

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,21 @@ use axum::{
     routing::get,
     Router,
 };
+use axum::Server;
+use std::net::SocketAddr;
+use hello_cargo::app;
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/hello", get(|| async { "Hello, World!" }));
+    let app = app();
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("Listening on {}", addr);
+    Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
